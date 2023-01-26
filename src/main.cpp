6cd74702873e87d6df9a9cb6565a089e0e4161d9
Craig Moore <cdm21d@fsu.edu>
2023-01-26 13:05:29 -0500

    Set values for kitchen light

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
+const int speakerPin = 5;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(speakerPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int reading  = analogRead(A0);
+
+  // the map() function takes 5 arguments: the number we want to scale,
+  // the minimum and maximum values that we are getting,
+  // and the minimum and maximum values of the range we'd like
+  int frequency = map(reading, 0, 200, 500, 1000);
+
+  // Makes tone on speaker
+  tone(speakerPin, frequency);
+
+  Serial.print(reading);
+  Serial.print(" -> ");
+  Serial.println(frequency);
 }
