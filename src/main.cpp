dedf45ee757a86a477b6956aeda61d2857a5191e
Craig Moore <cdm21d@fsu.edu>
2023-01-26 13:05:45 -0500

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ void loop() {
   // the map() function takes 5 arguments: the number we want to scale,
   // the minimum and maximum values that we are getting,
   // and the minimum and maximum values of the range we'd like
+  // Kitchen light
   int frequency = map(reading, 0, 200, 500, 1000);
 
   // Makes tone on speaker
